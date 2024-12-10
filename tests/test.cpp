8c29b211a202e260d6006334f0aeaeab0f1cd824
TimurN4 <nogaevtimur@gmail.com>
2024-12-10 22:19:55 +0300

    with tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,110 +1,93 @@
 #include <gtest/gtest.h>
-#include "../include/NPCFactory.h"
 #include "../include/NPC.h"
-#include "../include/BattleVisitor.h"
+#include "../include/NPCFactory.h"
 #include "../include/NPCManager.h"
 
-TEST(NPCTest, Constructor) {
-    Bandit bandit("Bandit1", 100, 100);
-    EXPECT_EQ(bandit.getName(), "Bandit1");
-    EXPECT_EQ(bandit.getX(), 100);
-    EXPECT_EQ(bandit.getY(), 100);
-    EXPECT_TRUE(bandit.isAlive());
-}
-
-TEST(NPCTest, SetAlive) {
-    Bandit bandit("Bandit1", 100, 100);
-    bandit.setAlive(false);
-    EXPECT_FALSE(bandit.isAlive());
-}
-
-
+// Тест для создания NPC через NPCFactory
 TEST(NPCFactoryTest, CreateNPC) {
-    auto bandit = NPCFactory::createNPC("Bandit", "Bandit1", 100, 100);
-    EXPECT_NE(bandit, nullptr);
+    auto bandit = NPCFactory::createNPC("Bandit", "Bandit1", 10, 20);
+    ASSERT_NE(bandit, nullptr);
     EXPECT_EQ(bandit->getType(), "Bandit");
     EXPECT_EQ(bandit->getName(), "Bandit1");
-    EXPECT_EQ(bandit->getX(), 100);
-    EXPECT_EQ(bandit->getY(), 100);
+    EXPECT_EQ(bandit->getX(), 10);
+    EXPECT_EQ(bandit->getY(), 20);
+    EXPECT_TRUE(bandit->isAlive());
 }
 
-TEST(NPCFactoryTest, LoadNPC) {
-    std::ifstream file("npcs.txt");
-    auto npc = NPCFactory::loadNPC(file);
-    EXPECT_NE(npc, nullptr);
-    EXPECT_EQ(npc->getType(), "Bandit");
-    EXPECT_EQ(npc->getName(), "Bandit1");
-    EXPECT_EQ(npc->getX(), 100);
-    EXPECT_EQ(npc->getY(), 100);
+// Тест для передвижения NPC
+TEST(NPCManagerTest, MoveNPC) {
+    NPCManager manager;
+    manager.addNPC("Bandit", "Bandit1", 10, 20);
+    manager.addNPC("WanderingKnight", "Knight1", 30, 40);
+
+    auto& npcs = manager.getNPCs();
+    auto& bandit = npcs[0];
+    auto& knight = npcs[1];
+
+    // Сохраняем начальные координаты
+    int initialBanditX = bandit->getX();
+    int initialBanditY = bandit->getY();
+    int initialKnightX = knight->getX();
+    int initialKnightY = knight->getY();
+
+    // Передвигаем NPC
+    manager.moveNPCs();
+
+    // Проверяем, что координаты изменились
+    EXPECT_NE(bandit->getX(), initialBanditX);
+    EXPECT_NE(bandit->getY(), initialBanditY);
+    EXPECT_NE(knight->getX(), initialKnightX);
+    EXPECT_NE(knight->getY(), initialKnightY);
 }
 
+// Тест для боев между NPC
+TEST(NPCManagerTest, BattleNPC) {
+    NPCManager manager;
+    manager.addNPC("Bandit", "Bandit1", 10, 20);
+    manager.addNPC("WanderingKnight", "Knight1", 12, 22); // Близко к Bandit
 
-TEST(BattleVisitorTest, VisitBandit) {
-    NPCManager npcManager;
-    npcManager.addNPC("Bandit", "Bandit1", 100, 100);
-    npcManager.addNPC("Elf", "Elf1", 100, 100);
+    auto& npcs = manager.getNPCs();
+    auto& bandit = npcs[0];
+    auto& knight = npcs[1];
 
-    BattleVisitor battleVisitor(100, npcManager.getNPCs());
-    battleVisitor.visit(dynamic_cast<Bandit&>(*npcManager.getNPCs()[0]));
+    // Проверяем, что оба NPC живы до боя
+    EXPECT_TRUE(bandit->isAlive());
+    EXPECT_TRUE(knight->isAlive());
 
-    EXPECT_FALSE(npcManager.getNPCs()[1]->isAlive());
+    // Запускаем бой
+    manager.battleNPCs();
+
+    // Проверяем, что один из NPC убит
+    EXPECT_TRUE(bandit->isAlive() || knight->isAlive());
+    EXPECT_FALSE(bandit->isAlive() && knight->isAlive());
 }
 
-TEST(BattleVisitorTest, VisitWanderingKnight) {
-    NPCManager npcManager;
-    npcManager.addNPC("WanderingKnight", "Knight1", 100, 100);
-    npcManager.addNPC("Bandit", "Bandit1", 100, 100);
+// Тест для вывода карты
+TEST(NPCManagerTest, PrintMap) {
+    testing::internal::CaptureStdout();
 
-    BattleVisitor battleVisitor(100, npcManager.getNPCs());
-    battleVisitor.visit(dynamic_cast<WanderingKnight&>(*npcManager.getNPCs()[0]));
+    NPCManager manager;
+    manager.addNPC("Bandit", "Bandit1", 10, 20);
+    manager.addNPC("WanderingKnight", "Knight1", 30, 40);
 
-    EXPECT_FALSE(npcManager.getNPCs()[1]->isAlive());
+    manager.printMap();
+
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output.find("Bandit1"), std::string::npos);
+    EXPECT_NE(output.find("Knight1"), std::string::npos);
 }
 
-TEST(BattleVisitorTest, VisitElf) {
-    NPCManager npcManager;
-    npcManager.addNPC("Elf", "Elf1", 100, 100);
-    npcManager.addNPC("WanderingKnight", "Knight1", 100, 100);
+// Тест для вывода списка выживших
+TEST(NPCManagerTest, PrintSurvivors) {
+    testing::internal::CaptureStdout();
 
-    BattleVisitor battleVisitor(100, npcManager.getNPCs());
-    battleVisitor.visit(dynamic_cast<Elf&>(*npcManager.getNPCs()[0]));
+    NPCManager manager;
+    manager.addNPC("Bandit", "Bandit1", 10, 20);
+    manager.addNPC("WanderingKnight", "Knight1", 30, 40);
 
-    EXPECT_FALSE(npcManager.getNPCs()[1]->isAlive());
-}
+    manager.printSurvivors();
 
-
-TEST(NPCManagerTest, AddNPC) {
-    NPCManager npcManager;
-    npcManager.addNPC("Bandit", "Bandit1", 100, 100);
-    EXPECT_EQ(npcManager.getNPCs().size(), 1);
-    EXPECT_EQ(npcManager.getNPCs()[0]->getType(), "Bandit");
-    EXPECT_EQ(npcManager.getNPCs()[0]->getName(), "Bandit1");
-    EXPECT_EQ(npcManager.getNPCs()[0]->getX(), 100);
-    EXPECT_EQ(npcManager.getNPCs()[0]->getY(), 100);
-}
-
-TEST(NPCManagerTest, SaveAndLoadNPCs) {
-    NPCManager npcManager;
-    npcManager.addNPC("Bandit", "Bandit1", 100, 100);
-    npcManager.saveNPCs("npcs_test.txt");
-
-    NPCManager npcManager2;
-    npcManager2.loadNPCs("npcs_test.txt");
-
-    EXPECT_EQ(npcManager2.getNPCs().size(), 1);
-    EXPECT_EQ(npcManager2.getNPCs()[0]->getType(), "Bandit");
-    EXPECT_EQ(npcManager2.getNPCs()[0]->getName(), "Bandit1");
-    EXPECT_EQ(npcManager2.getNPCs()[0]->getX(), 100);
-    EXPECT_EQ(npcManager2.getNPCs()[0]->getY(), 100);
-}
-
-TEST(NPCManagerTest, StartBattle) {
-    NPCManager npcManager;
-    npcManager.addNPC("Bandit", "Bandit1", 100, 100);
-    npcManager.addNPC("Elf", "Elf1", 100, 100);
-
-    npcManager.startBattle(100);
-
-    EXPECT_EQ(npcManager.getNPCs().size(), 1);
-    EXPECT_EQ(npcManager.getNPCs()[0]->getType(), "Bandit");
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output.find("Bandit1"), std::string::npos);
+    EXPECT_NE(output.find("Knight1"), std::string::npos);
 }
